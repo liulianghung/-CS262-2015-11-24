769e77d236db9859de6e97b5f530642ddb7343d9
liulianghung <liulianghung@gmail.com>
2015-12-04 14:34:06 +0800

    test hello world

---
+++ Example01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("aaaa22");
+            Console.WriteLine("bbbb222");
+            Student aStudent = new Student();
+            aStudent.name = "aaabbb";
+            aStudent.phone = "123456789";
+            Console.WriteLine("{0}{1}", aStudent.name, aStudent.phone
+                );
+        }
+    }
+}
---
+++ Example01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Student
+    {
+        //public string Name { get; set; } //大寫開頭變數,接get,set,不用加分號
+        //public string Phone { get; set; }
+        public string name;  //小寫變數,則不能寫get,set,後面要加分號
+
+        public string phone;
+    }
+}
